c8bd17bc26df2ee94f8b8e6c5b35e839302b721c
zawlinnnaing <zawlinnnaing0018@gmail.com>
2020-02-03 18:07:06 +0630

    change url

--- modules/app-axios.js
+++ modules/app-axios.js
@@ -3,6 +3,7 @@ import axios from "axios";
 axios.defaults.headers.common["Content-Type"] = "application/json";
 // axios.defaults.headers.common["Access-Control-Allow-Origin"] = "*";
 axios.defaults.baseURL = process.env.VUE_APP_SERVER_URL;
+console.log(axios.defaults.baseURL);
 axios.defaults.withCredentials = true;
 
 export default axios;
