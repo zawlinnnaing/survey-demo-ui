da425aa7c4e208c4b52a95cbc6d2135d858d6fa4
zawlinnnaing <zawlinnnaing0018@gmail.com>
2020-02-04 21:25:08 +0630

    init file

---
+++ src/components/Error.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="jumbotron jumbotron-fluid">
+    <div class="container">
+      <ul class="list-group">
+        <li
+          class="list-group-item list-group-danger"
+          v-for="(message, index) in messages"
+          :key="index"
+        >
+          {{ message }}
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
+
+<script>
+export default {
+  computed() {
+    return {
+      messages() {
+        return this.$store.state.error.messages;
+      }
+    };
+  }
+};
+</script>
